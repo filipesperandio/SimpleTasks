4f42a3ae46f25a6603cd4411ce5c703bc6514cac
Filipe Esperandio <filipesperandio@gmail.com>
2015-10-07 09:47:20 -0300

    WIP TaskList concept

--- src/task.list.js
+++ src/task.list.js
@@ -1,8 +1,36 @@
-function TaskList () {
+function TaskListFactory (firebase, firebaseArray, user) {
 
-  return {
-    tasks : function () { return []; }
-  };
+  function TaskList (name) {
+    var rootPath = 'user/' + user.uid + '/tasklists/' + name;
+    var taskListRef = firebase.child(rootPath);
+    var tasks = firebaseArray(taskListRef);
+
+    function add (task) {
+      tasks.$add(task);
+    }
+
+    function remove (task) {
+      tasks.$remove(task);
+    }
+
+    function save (task) {
+      tasks.$save(task);
+    }
+
+    function all () {
+      return tasks; 
+    }
+
+    return {
+      name: function () { return name; },
+      all : all,
+      add: add,
+      remove: remove,
+      save: save
+    };
+  }
+
+  return TaskList;
 }
 
-module.exports = TaskList;
+module.exports = TaskListFactory;
--- test/task.list.test.js
+++ test/task.list.test.js
@@ -1,10 +1,32 @@
 describe('Task List', function () {
-  var TaskList = require('src/task.list');
+  var TaskListFactory = require('src/task.list');
+  var firebase = { child: stub() };
+  var firebaseArrayMock = { $add: sinon.spy(), $remove: stub(), $save: stub() }
+  var firebaseArray = stub().returns(firebaseArrayMock);
+  var user = {uid:123};
+  var task = {title: 'newtask'};
 
-  it('holds a list of tasks', function () {
-    var taskList = new TaskList()
-    assert.deepEqual(taskList.tasks(), []);
+  var TaskList = new TaskListFactory(firebase, firebaseArray, user);
+
+  var taskList;
+
+  beforeEach(function () {
+    taskList = new TaskList('newlist');
+  });
+
+  it('specify which list', function () {
+    assert.equal(taskList.name(), 'newlist');
+    assert.calledWith(firebase.child, 'user/123/tasklists/newlist');
+  });
+
+  it('adds task to the list', function () {
+    taskList.add(task);
+    assert.calledWith(firebaseArrayMock.$add, task);
+  });
+
+  it('lists all tasks from firebase', function () {
+    var all = taskList.all();
+    assert.equal(all, firebaseArrayMock);
   });
 
 });
-
