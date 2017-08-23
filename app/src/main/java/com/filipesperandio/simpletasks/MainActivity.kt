8c75bd92987724b62f1c82d5a7dddd84b0f813a0
Sergio <sertom@gmail.com>
2017-08-23 19:36:08 -0300

    fixed indentation

--- app/src/main/java/com/filipesperandio/simpletasks/MainActivity.kt
+++ app/src/main/java/com/filipesperandio/simpletasks/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-            tasks.presenterFactory(TaskPresenterFactory())
+        tasks.presenterFactory(TaskPresenterFactory())
 
         
         tasksBus.subscribe { tasks.append(it) }
