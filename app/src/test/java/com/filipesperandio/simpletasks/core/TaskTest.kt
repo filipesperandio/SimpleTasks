af25d7fc12a1076abfefad89433bea995be85193
Filipe Esperandio <filipesperandio@gmail.com>
2017-09-13 19:25:53 -0300

    Fix test

--- app/src/test/java/com/filipesperandio/simpletasks/core/TaskTest.kt
+++ app/src/test/java/com/filipesperandio/simpletasks/core/TaskTest.kt
@@ -2,12 +2,14 @@ package com.filipesperandio.simpletasks.core
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import java.util.*
 
 class TaskTest {
     @Test
     fun createTask() {
-        val task = Task(title = "Task1", done = false, createdAt = 1462421108736)
+        val task = Task(title = "Task1", done = false, createdAt = Date())
 
-        assertThat(task).isEqualTo(Task(title="Task1", done=false, createdAt = 1462421108736))
+        assertThat(task.id.toString()).isNotBlank()
+        assertThat(task.done).isFalse()
     }
 }
