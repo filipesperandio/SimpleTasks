8f7cf1269a71663dde6da37ea8a32c4930d36e98
Filipe Esperandio <filipesperandio@gmail.com>
2018-02-07 22:47:00 -0200

    Move to kotlin subdir

--- kotlin/app/src/test/java/com/filipesperandio/simpletasks/core/TaskTest.kt
+++ kotlin/app/src/test/java/com/filipesperandio/simpletasks/core/TaskTest.kt
@@ -2,13 +2,17 @@ package com.filipesperandio.simpletasks.core
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+
 import java.util.*
+import java.util.regex.Pattern
 
 class TaskTest {
     @Test
     fun createTask() {
         val task = Task(title = "Task1", done = false, createdAt = Date())
 
+        Pattern
+        Scanner
         assertThat(task.id.toString()).isNotBlank()
         assertThat(task.done).isFalse()
     }
